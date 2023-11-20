fb4a96bd4eb51a218780d948d2dc70cc95298217
Kapil Motwani <motwanikapil1@gmail.com>
2023-11-21 05:05:52 +0530

    latest commit on 21/11/23

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,13 @@
 const http = require("http")
 require("dotenv").config()
-const { mongoConnect } = require("./services/mongo")
 const app = require("./app")
-
+const { mongoConnect } = require("./services/mongo")
 const { loadPlanetsData } = require("./models/planets.model")
 const { loadLaunchesData } = require("./models/launches.model")
 
-const PORT = process.env.PORT || 8000
-
+const PORT = 8000
+const MONGO_URL =
+  "mongodb+srv://kapilmotwani320:K%40pil1010@cluster0.lctioa2.mongodb.net/?retryWrites=true&w=majority"
 const server = http.createServer(app)
 
 async function startServer() {
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose")
 
-const MONGO_URL = process.env.MONGO_URL
+// const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = "mongodb+srv://kapilmotwani320:K%40pil1010@cluster0.lctioa2.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!")
 })
 
 mongoose.connection.on("error", (err) => {
-  console.error(err)
+  // console.error(err)
 })
 
 async function mongoConnect() {
